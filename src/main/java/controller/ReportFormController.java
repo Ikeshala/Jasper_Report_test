8d11ecab480c0f6a53b0f497a8f67462a0940fea
Ikeshala <ikeshala.peiris@gmail.com>
2023-12-11 00:53:27 +0530

    Created UIs, connected the database, initialized DBConnection, and configured Maven to run the project.

---
+++ src/main/java/controller/ReportFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ReportFormController {
+
+    @FXML
+    void ItemReportButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerReportButtonOnAction(ActionEvent event) {
+    }
+
+}
